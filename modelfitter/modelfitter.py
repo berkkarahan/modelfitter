18eb768b26c18ee50b658a46fe78582507f60475
berkkarahan <berkkarahan00@gmail.com>
2019-03-17 12:09:14 +0300

    Update modelfitter.py

--- modelfitter/modelfitter.py
+++ modelfitter/modelfitter.py
@@ -1,6 +1,6 @@
+
 import threading
 
-import numpy as np
 from sklearn.base import clone
 
 class BaseCVFitter:
@@ -17,7 +17,7 @@ class BaseCVFitter:
         self._score = []
 
     @staticmethod
-    def _fit(mdl, x, y):
+    def _fit(mdl, x, y, **kwargs):
         return mdl.fit(x, y)
 
     @staticmethod
@@ -27,6 +27,18 @@ class BaseCVFitter:
             infoldpreds.append(m.predict(x))
         infoldpreds = np.hstack(infoldpreds)
         return infoldpreds
+    
+    @staticmethod
+    def _predict_proba(model_list, x):
+        infoldpreds = []
+        for m in model_list:
+            infoldpreds.append(m.predict_proba(x))
+        infoldpreds = np.hstack(infoldpreds)
+        return infoldpreds
+        
+    @property
+    def models(self):
+        return self._modellist
 
 
 class CVModelFit(BaseCVFitter):
@@ -37,13 +49,13 @@ class CVModelFit(BaseCVFitter):
             modellist.append(cloned_mdl)
         super().__init__(X=X, y=y, models_list=modellist, cvgen=cvgen)
 
-    def fit(self):
+    def fit(self, **kwargs):
         for i, tpl in enumerate(self.cvgen.split(self.x, self.y)):
             tr_i, ho_i = tpl
             self._train_ind.append(tr_i)
             self._holdout_ind.append(ho_i)
             cloned_mdl = self._modellist[i]
-            self._threadlist.append(threading.Thread(target=self._fit, args=(cloned_mdl, self.x[tr_i], self.y[tr_i],)))
+            self._threadlist.append(threading.Thread(target=self._fit, args=(cloned_mdl, self.x[tr_i], self.y[tr_i],), kwargs=kwargs))
 
         for t in self._threadlist:
             t.start()
@@ -53,8 +65,11 @@ class CVModelFit(BaseCVFitter):
 
         print("Model fitting complete.")
 
-    def predict(self, x):
-        infoldpreds = self._predict(self._modellist, x)
+    def predict(self, x, type='default'):
+        if type == 'default':
+            infoldpreds = self._predict(self._modellist, x)
+        elif type == 'proba':
+            infoldpreds = self._predict_proba(self._modellist, x)
         return np.mean(infoldpreds, axis=1)
 
     def score(self, skscorer):
@@ -62,4 +77,4 @@ class CVModelFit(BaseCVFitter):
             m = self._modellist[i]
             self._predictions.append(m.predict(self.x[ind_tup[1]]))
             self._score.append(skscorer(self.y[ind_tup[1]], self._predictions[i]))
-        print("Average score for the given scorer for models are: {}".format(np.mean(self._score)))
+            print("Average score for the given scorer for models are: {}".format(np.mean(self._score)))
